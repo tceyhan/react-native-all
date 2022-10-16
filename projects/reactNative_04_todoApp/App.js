a013c1a338d64bcc6643edf3cfb7fbd4ebf99736
tceyhan <55tc155@gmail.com>
2022-10-16 16:13:52 +0300

    app js up

--- projects/reactNative_04_todoApp/App.js
+++ projects/reactNative_04_todoApp/App.js
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
