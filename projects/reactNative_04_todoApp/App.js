0f99eb679af46a313c1dfdad77540097cd31d9c7
tceyhan <55tc155@gmail.com>
2022-10-16 23:42:23 +0300

    reactNative | Todo app start

--- projects/reactNative_04_todoApp/App.js
+++ projects/reactNative_04_todoApp/App.js
@@ -1,10 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <FlatList renderItem={todoRender} />
     </View>
   );
 };
