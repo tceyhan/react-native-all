17777c60920303bcab189aa0bb26bde7662440e3
tceyhan <55tc155@gmail.com>
2022-10-16 05:17:35 +0300

    music app started

--- projects/reactNative_03_musicApp/App.js
+++ projects/reactNative_03_musicApp/App.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import music_data from './src/assets/music-data.json';
 const App = () => {
   return (
-    <View>
-      <Text>App ..</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View>
+        <FlatList
+          data={music_data}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
