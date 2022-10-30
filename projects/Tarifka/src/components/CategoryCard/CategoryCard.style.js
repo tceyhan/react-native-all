73ded805c99c53e3f057d5174eda83377674b1fd
tceyhan <55tc155@gmail.com>
2022-10-30 05:54:38 +0300

    categorycard style up

--- projects/Tarifka/src/components/CategoryCard/CategoryCard.style.js
+++ projects/Tarifka/src/components/CategoryCard/CategoryCard.style.js
@@ -1,25 +1,24 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        display: 'flex',
-        backgroundColor: '#faf0e6',
-        margin: 5,
-        paddingLeft: 10,
-        borderRadius: 10,
-        borderTopLeftRadius: 50,
-        borderBottomLeftRadius: 50,
-        flexDirection: 'row',
-        alignItems: 'center',
-        
-      },
-      text: {color: 'black', fontSize: 25},
-      image: {
-        backgroundColor: '#faf0e6',
-        width: 120,
-        height: 120,
-        marginRight: 10,
-        borderRadius: 50,
-        resizeMode: 'contain',
-      },
-})
+  container: {
+    display: 'flex',
+    backgroundColor: 'gainsboro',
+    margin: 5,
+    paddingLeft: 10,
+    borderRadius: 10,
+    borderTopLeftRadius: 50,
+    borderBottomLeftRadius: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {color: 'black', fontSize: 25},
+  image: {
+    backgroundColor: 'gainsboro',
+    width: 120,
+    height: 120,
+    marginRight: 10,
+    borderRadius: 50,
+    resizeMode: 'contain',
+  },
+});
