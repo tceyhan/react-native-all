1ecf2f606b8839553d4c91e81686049a25cae602
tceyhan <55tc155@gmail.com>
2022-11-22 04:28:21 +0300

    process env added

--- projects/jobfind/src/redux/store/index.js
+++ projects/jobfind/src/redux/store/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable prettier/prettier */
-import {createStore,applyMiddleware} from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from '@redux-devtools/extension';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+let store;
+if (process.env.NODE_ENV === 'developement') {
+  store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+} else {
+  store = createStore(rootReducer, applyMiddleware(thunk));
+}
 
 export default store;
-
