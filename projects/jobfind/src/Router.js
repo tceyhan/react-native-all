900fa62911f29088a46599a76c76d7a529fe631c
tceyhan <55tc155@gmail.com>
2022-11-27 07:01:35 +0300

    axios version down

--- projects/jobfind/src/Router.js
+++ projects/jobfind/src/Router.js
@@ -1,9 +1,9 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import store from './redux/store';
 import {Provider} from 'react-redux';
 import DrawerNavigation from './pages/navigations/DrawerNavigation';
-
 
 const Router = () => {
   return (
